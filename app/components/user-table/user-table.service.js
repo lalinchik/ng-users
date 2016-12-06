947d846f4c3fc2b3e2f85434b4503c5d15297b78
alina <smaylik2410@gmail.com>
2016-12-06 21:13:17 +0200

    Refactor code without $q

--- app/components/user-table/user-table.service.js
+++ app/components/user-table/user-table.service.js
@@ -1,16 +1,10 @@
 'use strict';
 
-angular.module('userTable').factory('UserTableService', function ($http, $q) {
+angular.module('userTable').factory('UserTableService', function ($http) {
     return {
-        fetchUsers: (data) => {
-            const deferred = $q.defer();
-            const {limit, gender, nat} = data;
-
-            $http.get(`https://randomuser.me/api/?results=${limit }&gender=${ gender }&nat=${nat}`)
-                .success(response => deferred.resolve(response.results))
-                .error(() => console.log('Oh, sorry'));
-
-            return deferred.promise;
+        fetchUsers: ({limit, gender, nat}) => {
+            return $http.get(`https://randomuser.me/api/?results=${limit}&gender=${gender}&nat=${nat}`)
+                .then(response => response.data.results);
         }
     }
 });

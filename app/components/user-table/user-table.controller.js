a2695e45db9feb462f9c9bf278411832ad1e4de6
alina <smaylik2410@gmail.com>
2017-01-14 13:42:14 +0200

    Add functionality in pop-up

--- app/components/user-table/user-table.controller.js
+++ app/components/user-table/user-table.controller.js
@@ -1,8 +1,20 @@
-function UserTableController() {
+function UserTableController(UserTableService) {
   this.isPopupShow = false;
   this.columnList = ['Avatar', 'Name', 'Gender', 'Nat'];
 
   this.openPopup = function () {
     this.isPopupShow = !this.isPopupShow;
+  };
+
+  this.toggleColumn = function (columnName) {
+      if (columnName === 'Avatar') {
+        this.isAvatarColumnHide = !this.isAvatarColumnHide;
+      } else if (columnName === 'Name') {
+        this.isNameColumnHide = !this.isNameColumnHide;
+      } else if (columnName === 'Gender') {
+        this.isGenderColumnHide = !this.isGenderColumnHide;
+      } else if (columnName === 'Nat') {
+        this.isNatColumnHide = !this.isNatColumnHide;
+      }
   }
 }

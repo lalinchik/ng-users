8e16a67beaafd7edc1ba032f9c27dd6e30e1eb17
alina <smaylik2410@gmail.com>
2017-01-18 19:48:48 +0200

    Add sort icon to the table header

--- app/components/user-table/user-table.controller.js
+++ app/components/user-table/user-table.controller.js
@@ -1,11 +1,11 @@
 function UserTableController() {
   this.isPopupShow = false;
   this.columnList = [
-    {title: 'Avatar', hidden: false, renderer: (user) => `<img class="avatar" src="${user.picture.thumbnail}">`},
-    {title: 'Name', hidden: false, renderer: (user) => `${user.name.title} ${user.name.first} ${user.name.last}`},
-    {title: 'Gender', hidden: false, renderer: (user) => user.gender},
-    {title: 'Nationality', hidden: false, renderer: (user) => user.nat},
-    {title: 'Email', hidden: false, renderer: (user) => `<a href="mailto:${user.email}">${user.email}</a>`}
+    {title: 'Avatar', hidden: false, sortable: false, renderer: (user) => `<img class="avatar" src="${user.picture.thumbnail}">`},
+    {title: 'Name', hidden: false, sortable: true, renderer: (user) => `${user.name.title} ${user.name.first} ${user.name.last}`},
+    {title: 'Gender', hidden: false, sortable: false, renderer: (user) => user.gender},
+    {title: 'Nationality', hidden: false, sortable: true,  renderer: (user) => user.nat},
+    {title: 'Email', hidden: false, sortable: false, renderer: (user) => `<a href="mailto:${user.email}">${user.email}</a>`}
   ];
 
   this.openPopup = function () {

638b57733070a83a2a2b55906059e75dc17a0dbb
alina <smaylik2410@gmail.com>
2017-01-19 21:03:40 +0200

    Refactor code

--- app/components/user-table/user-table.controller.js
+++ app/components/user-table/user-table.controller.js
@@ -1,11 +1,28 @@
 function UserTableController() {
   this.isPopupShow = false;
   this.columnList = [
-    {title: 'Avatar', hidden: false, sortable: false, renderer: (user) => `<img class="avatar" src="${user.picture.thumbnail}">`},
-    {title: 'Name', hidden: false, sortable: true, value: (user) => `${user.name.first} ${user.name.last}`, renderer: (user) => `${user.name.first} ${user.name.last}`},
+    {
+      title: 'Avatar',
+      hidden: false,
+      sortable: false,
+      renderer: (user) => `<img class="avatar" src="${user.picture.thumbnail}">`
+    },
+    {
+      title: 'Name',
+      hidden: false,
+      sortable: true,
+      value: (user) => `${user.name.first} ${user.name.last}`,
+      renderer: (user) => `${user.name.first} ${user.name.last}`
+    },
     {title: 'Gender', hidden: false, sortable: true, value: (user) => user.gender, renderer: (user) => user.gender},
-    {title: 'Nationality', hidden: false, sortable: true,  value: (user) => user.nat, renderer: (user) => user.nat},
-    {title: 'Email', hidden: false, sortable: true, value: (user) => user.email, renderer: (user) => `<a href="mailto:${user.email}">${user.email}</a>`}
+    {title: 'Nationality', hidden: false, sortable: true, value: (user) => user.nat, renderer: (user) => user.nat},
+    {
+      title: 'Email',
+      hidden: false,
+      sortable: true,
+      value: (user) => user.email,
+      renderer: (user) => `<a href="mailto:${user.email}">${user.email}</a>`
+    }
   ];
 
   this.openPopup = function () {
@@ -19,7 +36,7 @@ function UserTableController() {
 
   this.sort = function (column, direction) {
     this.users.sort((userA, userB) => {
-      if(direction === 'asc') {
+      if (direction === 'asc') {
         return column.value(userA).localeCompare(column.value(userB));
       } else {
         return -column.value(userA).localeCompare(column.value(userB));

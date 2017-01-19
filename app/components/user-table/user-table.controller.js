fcd690739f5f6c14caadad179b82854da467be35
alina <smaylik2410@gmail.com>
2017-01-19 20:44:12 +0200

    Add sort to  the table

--- app/components/user-table/user-table.controller.js
+++ app/components/user-table/user-table.controller.js
@@ -2,10 +2,10 @@ function UserTableController() {
   this.isPopupShow = false;
   this.columnList = [
     {title: 'Avatar', hidden: false, sortable: false, renderer: (user) => `<img class="avatar" src="${user.picture.thumbnail}">`},
-    {title: 'Name', hidden: false, sortable: true, renderer: (user) => `${user.name.title} ${user.name.first} ${user.name.last}`},
-    {title: 'Gender', hidden: false, sortable: false, renderer: (user) => user.gender},
-    {title: 'Nationality', hidden: false, sortable: true,  renderer: (user) => user.nat},
-    {title: 'Email', hidden: false, sortable: false, renderer: (user) => `<a href="mailto:${user.email}">${user.email}</a>`}
+    {title: 'Name', hidden: false, sortable: true, value: (user) => `${user.name.first} ${user.name.last}`, renderer: (user) => `${user.name.first} ${user.name.last}`},
+    {title: 'Gender', hidden: false, sortable: true, value: (user) => user.gender, renderer: (user) => user.gender},
+    {title: 'Nationality', hidden: false, sortable: true,  value: (user) => user.nat, renderer: (user) => user.nat},
+    {title: 'Email', hidden: false, sortable: true, value: (user) => user.email, renderer: (user) => `<a href="mailto:${user.email}">${user.email}</a>`}
   ];
 
   this.openPopup = function () {
@@ -15,5 +15,15 @@ function UserTableController() {
   this.toggleColumn = function (columnName) {
     const column = this.columnList.find(column => column.title === columnName);
     column.hidden = !column.hidden;
+  };
+
+  this.sort = function (column, direction) {
+    this.users.sort((userA, userB) => {
+      if(direction === 'asc') {
+        return column.value(userA).localeCompare(column.value(userB));
+      } else {
+        return -column.value(userA).localeCompare(column.value(userB));
+      }
+    });
   }
 }
